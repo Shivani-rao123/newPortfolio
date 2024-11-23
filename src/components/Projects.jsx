f6b369e95a1fcaba7365b4e7f7aa407141a66a05
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-11-23 12:45:08 +0530

    Initial commit of Next.js portfolio

---
+++ src/components/Projects.jsx
@@ -0,0 +1,86 @@
+"use client";
+import Image from "next/image";
+
+import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
+
+export function Projects() {
+  const cards = data.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
+
+  return (
+    <div className="w-full h-full py-20">
+      <h2
+        className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+        Recent projects
+      </h2>
+      <Carousel items={cards} />
+    </div>
+  );
+}
+
+const DummyContent = () => {
+  return (
+    <>
+      {[...new Array(1).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+            <p
+              className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                The first rule of Apple club is that you boast about Apple club.
+              </span>{" "}
+              Keep a journal, quickly jot down a grocery list, and take amazing
+              class notes. Want to convert those notes to text? No problem.
+              Langotiya jeetu ka mara hua yaar is ready to capture every
+              thought.
+            </p>
+            <Image
+              src="/images/Screenshot (410).png"
+              alt="Macbook mockup from Aceternity UI"
+              height={500}
+              width={500}
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain" />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const data = [
+  {
+    category: "Project 1",
+    title: "Placement website",
+    src: "/images/Screenshot (410).png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Project 2",
+    title: "Flappy bird game ",
+    src: "/images/Screenshot (424).png",
+    content: <DummyContent />,
+  },
+  {
+    category: "Project 3",
+    title: "Weather app",
+    src: "/images/Screenshot (36).png",
+    content: <DummyContent />,
+  },
+
+  {
+    category: "Project 4",
+    title: "Tweet generating app",
+    src: "",
+    content: <DummyContent />,
+  },
+  {
+    category: "project 4",
+    title: "Simple project with docker ",
+    src: "",
+    content: <DummyContent />,
+  },
+ 
+];
---
+++ src/components/Services.jsx
@@ -0,0 +1,120 @@
+import { cn } from "@/lib/utils";
+import {
+    Icon12Hours,
+  IconAdjustmentsBolt,
+  IconCloud,
+  IconCurrencyDollar,
+  IconEaseInOut,
+  IconHeart,
+  IconHelp,
+  IconRouteAltLeft,
+  IconTerminal2,
+} from "@tabler/icons-react";
+import { Cover } from "./ui/cover";
+
+export function Services() {
+  const features = [
+    {
+      title: "Front-End",
+      description:
+        "Experienced in building responsive interfaces using React, Next.js, TailwindCSS, and JavaScript with a focus on modern design and user experience",
+
+      icon: <IconTerminal2 />,
+    },
+    {
+      title: "Back-end",
+      description:
+        "Skilled in sql and also express js",
+      icon: <IconAdjustmentsBolt />,
+    },
+    {
+      title: "Graphic Design",
+      description:
+        "designed lot of user friendly websites using figma as well as react",
+      icon: <IconEaseInOut />,
+    },
+    {
+      title: "Dev ops",
+      description: "enthusiast in learning dev ops",
+      icon: <IconCloud />,
+    },
+    {
+      title: "Multi-talented",
+      description: "other than technical skills I am talented in singing and also have a creativity mindset",
+      icon: <IconRouteAltLeft />,
+    },
+    {
+      title: "24/7 hardworking",
+      description:
+        "I believe in giving 100% to whatever the work I am doing",
+      icon: <Icon12Hours/>,
+    },
+    {
+      title: "Block chain",
+      description:
+        "I have also developed interest in blockchain technology",
+      icon: <IconCurrencyDollar />,
+    },
+    {
+      title: "And everything else",
+      description: "I thrive on exploring new technologies where every discovery fuels my passion for innovation and growth ",
+      icon: <IconHeart />,
+    },
+  ];
+  return (
+    <div className="mt-[150px flex flex-col]">
+    <div className="px-8 flex flex-col justify-center items-center">
+    <h2 className="bg-clip-text  text-white  text-transparent text-start bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-5xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+           <Cover> Skills </Cover>
+          </h2>
+    </div>
+    (<div
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  relative z-10 py-10 max-w-7xl mx-auto">
+      {features.map((feature, index) => (
+        <Feature key={feature.title} {...feature} index={index} />
+      ))}
+    </div>)
+    </div>
+  );
+}
+
+const Feature = ({
+  title,
+  description,
+  icon,
+  index
+}) => {
+  return (
+    (<div
+      className={cn(
+        "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-800",
+        (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800",
+        index < 4 && "lg:border-b dark:border-neutral-800"
+      )}>
+      {index < 4 && (
+        <div
+          className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+      )}
+      {index >= 4 && (
+        <div
+          className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
+      )}
+      <div
+        className="mb-4 relative z-10 px-10 text-neutral-600 dark:text-neutral-400">
+        {icon}
+      </div>
+      <div className="text-lg font-bold mb-2 relative z-10 px-10">
+        <div
+          className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
+        <span
+          className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
+          {title}
+        </span>
+      </div>
+      <p
+        className="text-sm text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10">
+        {description}
+      </p>
+    </div>)
+  );
+};
