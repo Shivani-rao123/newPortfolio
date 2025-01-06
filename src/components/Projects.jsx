6160ec27a33ef2ec5af099e4b30069a137b71665
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-01-06 18:48:58 +0530

    ........

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -110,6 +110,40 @@ const DummyContent2 = () => {
   );
 };
 
+const DummyContent3 = () => {
+  return (
+    <>
+      {[...new Array(1).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+            <p
+              className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                The first rule of Apple club is that you boast about Apple club.
+              </span>{" "}
+              Keep a journal, quickly jot down a grocery list, and take amazing
+              class notes. Want to convert those notes to text? No problem.
+              Langotiya jeetu ka mara hua yaar is ready to capture every
+              thought.
+            </p>
+            <Image
+              src="/images/Docusphere.png"
+              src1="/images/ContentDoc.png"
+              src2="/images/FileContent.png"
+              alt="Macbook mockup from Aceternity UI"
+              height={500}
+              width={500}
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain" />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+
 
 
 const data = [
@@ -117,6 +151,8 @@ const data = [
     category: "Project 1",
     title: "Placement website",
     src: "/images/Screenshot (410).png",
+    src1 : "/images/ContentDoc.png",
+    src2 : "/images/FileContent.png",
     content: <DummyContent />,
   },
   {
@@ -134,13 +170,13 @@ const data = [
 
   {
     category: "Project 4",
-    title: "Tweet generating app",
-    src: "",
-    content: <DummyContent />,
+    title: "Docusphere",
+    src: "/images/Docusphere.png",
+   content: <DummyContent3/>,
   },
   {
     category: "project 4",
-    title: "Simple project with docker ",
+    title: "Tweet generating app ",
     src: "",
     content: <DummyContent />,
   },
